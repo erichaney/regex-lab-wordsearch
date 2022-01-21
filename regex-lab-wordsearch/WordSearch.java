543ed6d0c6d5ba05d5475a66a40ecfafa412a947
erichaney <ehaney@outlook.com>
2022-01-21 09:55:05 -0700

    Initial commit

---
+++ regex-lab-wordsearch/WordSearch.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+
+class WordSearch
+{
+    /**
+     * Add methods to this class to read the included text
+     * file with a scanner.
+     * 
+     * You will need to print some of your answers to new text files 
+     * with a printstream.
+     */
+    
+    static void question1()
+    {
+        
+    }
+}
